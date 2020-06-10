37e93e99df29b7ad192e24793a9e7fb71ea38ec9
Iulia Bivolaru <iulia.bivolaru10@gmail.com>
2020-06-10 17:25:40 +0200

    Export function

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,12 @@
 import { handleSubmit } from './js/weather';
+import { postWeatherAndGeoData } from './js/weather';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.scss';
 
 export {
-    handleSubmit
+    handleSubmit,
+    postWeatherAndGeoData
 }
 
 
